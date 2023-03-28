9c40bde502de05b4508e9354ef73ec0dce58ce4d
SAMOUDIAYMEN <samoudiayman777@gmail.com>
2023-03-28 00:10:50 +0000

    make s changes

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * _strcpy - has two arguments
- * @dest: first argument
- * @src: second argument
- * Return: nothing
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int l = 0;
+	int x = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for (; i < src[i]; i++)
-		dest[i] = '\0';
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
 	return (dest);
 }
