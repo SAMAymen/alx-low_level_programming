62cba6633f90d26b78fdb306876b967f777bfec4
SAMAymen <samoudiayman777@gmail.com>
2023-04-09 16:17:46 -0700

    done

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-c_files=$(ls *.c)
-for file in $c_files; do
-	    gcc -c $file
-    done
-ar rcs liball.a *.o
-rm *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
 
