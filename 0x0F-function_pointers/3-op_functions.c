6a21f40657ed8d5d9e8fe86a848f489e93ff3226
SAMOUDIAYMEN <samoudiayman777@gmail.com>
2023-04-20 17:18:49 +0000

    done

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,59 +1,59 @@
 #include "3-calc.h"
-
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 /**
- * op_add - addition for two integers
- * @a: first integer
- * @b: second integer
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the sum of a  and b.
+ * Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - difference for two integers
- * @a: first integer
- * @b: second integer
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: difference of a and b.
+ * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_div - a division function
- * @a: first integer
- * @b: second integer
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: result of the division of a and b.
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-
-/**
- * op_mul - finds the product
- * @a: integer
- * @b: integer
- *
- * Return: the product of a and b.
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_mod - finds remainder
- * @a: integer
- * @b: integer
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the remainder of the division of a by b.
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
