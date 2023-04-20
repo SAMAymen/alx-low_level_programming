bf1fe5120dad7d379beb2b5cbb846e45200a9d46
SAMOUDIAYMEN <samoudiayman777@gmail.com>
2023-04-20 18:56:45 +0000

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
- * print_name - print name using pointer to function
- * @name: string to add
+ * print_name - function that prints a name
+ * @name: name to print
  * @f: pointer to function
- * Return: nothing
- **/
+ *
+ * Return: Nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
-
-
 	f(name);
 }
