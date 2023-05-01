318af0b5a71a2eb2b821c627cf3c40936e2b2963
SAMAymen <samoudiayman777@outlook.com>
2023-05-02 00:06:14 +0100

    adding another file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
- * pop_listint - delete the head node and return its data
+ * pop_listint - deletes head of the node
+ *
  * @head: pointer to the first node
  *
  * Return: the head node's data(n)
  */
 int pop_listint(listint_t **head)
 {
+	int mydata;
+	listint_t *temp, *next;
+
 	if (*head == NULL)
 		return (0);
 
-	listint_t *temp = *head;
-	int data = temp->n;
-
-	*head = temp->next;
+	temp = *head;
+	mydata = temp->n;
+	next = temp->next;
 	free(temp);
+	*head = next;
 
-	return (data);
+	return (mydata);
 }
