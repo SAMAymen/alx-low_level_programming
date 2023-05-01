920781660415b985f971fe1c0997e87bc957639d
SAMAymen <samoudiayman777@outlook.com>
2023-05-01 22:38:05 +0100

    adding permission

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,10 +7,10 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 0;
+	size_t nodeCount = 0;
 	if (h == NULL)
 		return (0);
-	for (; h != NULL; h = h->next, node_count++)
+	for (; h != NULL; h = h->next, nodeCount++)
 		printf("%d\n", h->n);
-	return (node_count);
+	return (nodeCount);
 }
