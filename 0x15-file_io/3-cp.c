4e6a9b065a3ac41d009ad190cd4021adefa56b18
SAMAymen <samoudiayman777@outlook.com>
2023-05-09 13:44:20 +0100

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,92 +1,71 @@
 #include "main.h"
+#include <errno.h>
 
+#define BUFFER_SIZE 1024
+#define FILE_PERMS 0664
 #define READ_ERR "Error: Can't read from file %s\n"
 #define WRITE_ERR "Error: Can't write to %s\n"
 
-int open_file(const char *path, int flags, int mode);
-int copy_file(int from, int to);
-
 /**
- * cp - Copy the content of a file to another file.
- * @argc: Number of arguments.
- * @argv: Arguments vector.
- *
+ * main - check the code for Holberton School students.
+ * @argc: num of args
+ * @argv: args
  * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
-	int from, to, ret;
+	int from, to, w, r, on_close;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (EXIT_FAILURE);
+		exit(97);
 	}
 
-	from = open_file(argv[1], O_RDONLY, 0);
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMS);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]);
+		exit(99);
+	}
+
+	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
 		dprintf(STDERR_FILENO, READ_ERR, argv[1]);
-		return (EXIT_FAILURE);
+		exit(98);
 	}
 
-	to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (to == -1)
+	while ((r = read(from, buffer, BUFFER_SIZE)) > 0)
 	{
-		close(from);
-		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]);
-		return (EXIT_FAILURE);
+		w = write(to, buffer, r);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, WRITE_ERR, argv[2]);
+			exit(99);
+		}
 	}
 
-	ret = copy_file(from, to);
-
-	if (close(from) == -1 || close(to) == -1)
+	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-		return (EXIT_FAILURE);
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]);
+		exit(98);
 	}
 
-	return (ret);
-}
-
-/**
- * open_file - just Opens a file.
- * @path: The path to the file.
- * @flags: The flags to use when opening the file.
- * @mode: The mode to use when creating a new file.
- *
- * Return: The file descriptor, or -1 on failure.
- */
-int open_file(const char *path, int flags, int mode)
-{
-	int fd = open(path, flags, mode);
-
-	if (fd == -1)
-		return (-1);
-	return (fd);
-}
-
-/**
- * copy_file - Copies the contents of one file to another.
- * @from:  The file descriptor of the source file.
- * @to: The file descriptor of the destination file.
- *
- * Return: 0 on success, or -1 on failure.
- */
-int copy_file(int from, int to)
-{
-	char buffer[BUFSIZ];
-	ssize_t nread, nwritten;
-
-	while ((nread = read(from, buffer, BUFSIZ)) > 0)
+	on_close = close(from);
+	if (on_close == -1)
 	{
-		nwritten = write(to, buffer, nread);
-		if (nwritten == -1 || nwritten != nread)
-			return (-1);
+		perror("Error");
+		exit(100);
 	}
 
-	if (nread == -1)
-		return (-1);
+	on_close = close(to);
+	if (on_close == -1)
+	{
+		perror("Error");
+		exit(100);
+	}
 
 	return (0);
 }
