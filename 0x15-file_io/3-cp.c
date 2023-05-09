8898b6290956c41fd5462f907633c1d054ed3ae0
SAMAymen <samoudiayman777@outlook.com>
2023-05-09 14:03:38 +0100

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,7 @@
 #define FILE_PERMS 0664
 #define READ_ERR "Error: Can't read from file %s\n"
 #define WRITE_ERR "Error: Can't write to %s\n"
+#define CLOSE_ERR "Error: Can't close fd %d\n"
 
 /**
  * main - check the code for Holberton School students.
@@ -18,54 +19,32 @@ int main(int argc, char **argv)
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMS);
-	if (to == -1)
-	{
-		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]);
-		exit(99);
-	}
-
 	from = open(argv[1], O_RDONLY);
+
+	if (to == -1)
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
 	if (from == -1)
-	{
-		dprintf(STDERR_FILENO, READ_ERR, argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 
 	while ((r = read(from, buffer, BUFFER_SIZE)) > 0)
 	{
 		w = write(to, buffer, r);
 		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, WRITE_ERR, argv[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
 	}
 
 	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, READ_ERR, argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 
 	on_close = close(from);
 	if (on_close == -1)
-	{
-		perror("Error");
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, CLOSE_ERR, from), exit(100);
 
 	on_close = close(to);
 	if (on_close == -1)
-	{
-		perror("Error");
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, CLOSE_ERR, to), exit(100);
 
 	return (0);
 }
