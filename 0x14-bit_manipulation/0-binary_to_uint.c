22865c883617ca2f0ccb242e215c9145dc5d14b7
SAMAymen <samoudiayman777@outlook.com>
2023-07-07 21:59:35 +0100

    adding binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,25 +4,27 @@
 /**
  * binary_to_uint: convert a binary number to an unsigned int
  * @b: char var
+ *
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
 {
+	if (b == NULL)
+        {
+                return (0);
+        }
+
 	unsigned int converted_number = 0;
 	
-	if (*b == NULL)
-	{
-		return (0);
-	}
-
 	while (*b != '0')
 	{
 		if (*b != '0' && *b != '1')
 		{
 			return(0);
 		}
-		nb = (nb*2) + *b
+		converted_number = (converted_number * 2) + *b
 	}
-	return (nb)
+	b++
+	return (converted_number)
 }
 
