850740c8b72cb9e94f5359d9d141e954d9670440
SAMAymen <samoudiayman777@outlook.com>
2023-07-07 22:21:19 +0100

    adding binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,27 @@
 unsigned int binary_to_uint(const char *b)
 {
 	if (b == NULL)
-        {
-                return (0);
-        }
+	{
+
+		return (0);
+
+	}
 
 	unsigned int converted_number = 0;
-	
+
 	while (*b != '\0')
 	{
+
 		if (*b != '0' && *b != '1')
 		{
+
 			return(0);
+
 		}
+
 		converted_number = (converted_number * 2) + *b;
+
 		b++;
 	}
 	return (converted_number);
 }
-
